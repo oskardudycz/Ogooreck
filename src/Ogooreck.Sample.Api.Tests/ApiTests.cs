bb030d266d20c091e7ec744eeaf5e70dd36faecb
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-04-15 21:53:43 +0200

    Added first compiled version of Ogooreck Api implementation

---
+++ src/Ogooreck.Sample.Api.Tests/ApiTests.cs
@@ -0,0 +1,23 @@
+﻿using Ogooreck.API;
+using Xunit;
+using static Ogooreck.API.ApiSpecification;
+
+namespace Ogooreck.Sample.Api.Tests;
+
+public class Tests: IClassFixture<ApiSpecification<Program>>
+{
+    private ApiSpecification<Program> API;
+    public Tests(ApiSpecification<Program> api) => API = api;
+
+    [Fact]
+    public Task GetProducts() =>
+        API.Given(URL("/api/products"))
+            .When(GET())
+            .Then(OK());
+
+    // [Fact]
+    // public Task RegisterProduct() =>
+    //     API.Given(URL("/api/products"))
+    //         .When(GET())
+    //         .Then(OK());
+}
---
+++ src/Ogooreck.Sample.Api/Program.cs
@@ -0,0 +1,56 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+var products = new Dictionary<Guid, Product>();
+
+// Get Products
+app.MapGet("/api/products", (string? filter, int? page, int? pageSize) =>
+    products.Values
+        .Where(p => string.IsNullOrWhiteSpace(filter) || p.Name.Contains(filter) || p.Sku.Contains(filter))
+        .Skip(page ?? 0)
+        .Take(pageSize ?? 10)
+        .ToList()
+);
+
+
+// Get Product Details by Id
+app.MapGet("/api/products/{id}", (Guid id) =>
+    products.TryGetValue(id, out var product) ? Results.Ok(product) : Results.NotFound()
+);
+
+// Register new product
+app.MapPost("/api/products/", (RegisterProductRequest request) =>
+{
+    var productId = Guid.NewGuid();
+    var (sku, name) = request;
+
+    if (sku == null || name == null)
+        return Results.BadRequest();
+
+    products.Add(productId, new Product(
+        productId,
+        sku,
+        name
+    ));
+
+    return Results.Created($"/api/products/{productId}", productId);
+});
+
+app.Run();
+
+public record RegisterProductRequest(
+    string? SKU,
+    string? Name
+);
+
+public record Product(
+    Guid Id,
+    string Sku,
+    string Name
+);
+
+public partial class Program { }
