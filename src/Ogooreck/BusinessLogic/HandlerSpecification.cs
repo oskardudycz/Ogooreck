0d97569766e7bce0ab2e42a874bc9da986763cc5
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-10-07 13:10:20 +0200

    Added params for Whens for handler specification

--- src/Ogooreck/BusinessLogic/HandlerSpecification.cs
+++ src/Ogooreck/BusinessLogic/HandlerSpecification.cs
@@ -2,7 +2,6 @@
 
 namespace Ogooreck.BusinessLogic;
 #pragma warning disable CS1591
-
 public delegate DecideResult<TEvent, TState> Handler<TEvent, TState>(TState state);
 
 public class HandlerSpecification<TState>
@@ -28,53 +27,78 @@ public static class HandlerSpecificationExtensions
     public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
         When<TEvent, TState>(
             this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
-            Func<TState, TEvent> when
+            params Func<TState, TEvent>[] whens
         ) =>
-        given.When(state => DecideResult.For<TEvent, TState>(when(state)));
+        given.When(whens.Select(WhenMapping<TEvent, TState>.ToHandler).ToArray());
 
     public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
         When<TEvent, TState>(
             this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
-            Func<TState, TEvent[]> when
+            params Func<TState, TEvent[]>[] whens
         ) =>
-        given.When(state => DecideResult.For<TEvent, TState>(when(state)));
+        given.When(whens.Select(WhenMapping<TEvent, TState>.ToHandler).ToArray());
 
     public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
         When<TEvent, TState>(
             this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
-            Action<TState> when
+            params Action<TState>[] whens
         ) =>
-        given.When(state =>
+        given.When(whens.Select(WhenMapping<TEvent, TState>.ToHandler).ToArray());
+
+    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
+        When<TEvent, TState>(
+            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
+            params Func<TState>[] whens
+        ) =>
+        given.When(whens.Select(WhenMapping<TEvent, TState>.ToHandler).ToArray());
+
+    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
+        When<TEvent, TState>(
+            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
+            params Func<TState, TState>[] whens
+        ) =>
+        given.When(whens.Select(WhenMapping<TEvent, TState>.ToHandler).ToArray());
+
+
+    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
+        When<TEvent, TState>(
+            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
+            params Func<TEvent[]>[] whens
+        ) =>
+        given.When(whens.Select(WhenMapping<TEvent, TState>.ToHandler).ToArray());
+
+    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
+        When<TEvent, TState>(
+            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
+            params Func<TEvent>[] whens
+        ) =>
+        given.When(whens.Select(WhenMapping<TEvent, TState>.ToHandler).ToArray());
+}
+
+public static class WhenMapping<TEvent, TState>
+{
+    public static Handler<TEvent, TState> ToHandler(Func<TState, TState> when) =>
+        state => DecideResult.For<TEvent, TState>(when(state));
+
+    public static Handler<TEvent, TState> ToHandler(Func<TState, TEvent> when) =>
+        state => DecideResult.For<TEvent, TState>(when(state));
+
+    public static Handler<TEvent, TState> ToHandler(Func<TState> when) =>
+        _ => DecideResult.For<TEvent, TState>(when());
+
+    public static Handler<TEvent, TState> ToHandler(Action<TState> when) =>
+        state =>
         {
             when(state);
             return DecideResult.For<TEvent, TState>(state);
-        });
+        };
 
-    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
-        When<TEvent, TState>(
-            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
-            Func<TState> when
-        ) =>
-        given.When(_ => DecideResult.For<TEvent, TState>(when()));
+    public static Handler<TEvent, TState> ToHandler(Func<TState, TEvent[]> when) =>
+        state => DecideResult.For<TEvent, TState>(when(state));
 
-    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
-        When<TEvent, TState>(
-            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
-            Func<TState, TState> when
-        ) =>
-        given.When(state => DecideResult.For<TEvent, TState>(when(state)));
+    public static Handler<TEvent, TState> ToHandler(Func<TEvent[]> when) =>
+        _ => DecideResult.For<TEvent, TState>(when());
 
-    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
-        When<TEvent, TState>(
-            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
-            Func<TEvent[]> when
-        ) =>
-        given.When(_ => DecideResult.For<TEvent, TState>(when()));
-
-    public static WhenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState>
-        When<TEvent, TState>(
-            this GivenDeciderSpecificationBuilder<Handler<TEvent, TState>, TEvent, TState> given,
-            Func<TEvent> when
-        ) =>
-        given.When(_ => DecideResult.For<TEvent, TState>(when()));
+    public static Handler<TEvent, TState> ToHandler(Func<TEvent> when) =>
+        _ => DecideResult.For<TEvent, TState>(when());
 }
