8b31a000c1ed658554d61fa7ddd335c74b31f4e1
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-08-10 14:29:53 +0200

    Initial set of refactorings to make setup easier and more self explanatory

--- src/Ogooreck/API/ApiSpecification.cs
+++ src/Ogooreck/API/ApiSpecification.cs
@@ -11,22 +11,28 @@ using Ogooreck.Newtonsoft;
 
 namespace Ogooreck.API;
 
+public delegate HttpRequestMessage RequestTransform(HttpRequestMessage request);
+
 public static class ApiSpecification
 {
     ///////////////////
     ////   GIVEN   ////
     ///////////////////
-    public static Func<HttpRequestMessage, HttpRequestMessage> URI(string uri) =>
+
+    ///////////////////
+    ////   WHEN    ////
+    ///////////////////
+    public static RequestTransform URI(string uri) =>
         URI(new Uri(uri, UriKind.RelativeOrAbsolute));
 
-    public static Func<HttpRequestMessage, HttpRequestMessage> URI(Uri uri) =>
+    public static RequestTransform URI(Uri uri) =>
         request =>
         {
             request.RequestUri = uri;
             return request;
         };
 
-    public static Func<HttpRequestMessage, HttpRequestMessage> BODY<T>(T body) =>
+    public static RequestTransform BODY<T>(T body) =>
         request =>
         {
             request.Content = JsonContent.Create(body);
@@ -34,7 +40,7 @@ public static class ApiSpecification
             return request;
         };
 
-    public static Func<HttpRequestMessage, HttpRequestMessage> HEADERS(params Action<HttpRequestHeaders>[] headers) =>
+    public static RequestTransform HEADERS(params Action<HttpRequestHeaders>[] headers) =>
         request =>
         {
             foreach (var header in headers)
@@ -42,6 +48,29 @@ public static class ApiSpecification
                 header(request.Headers);
             }
 
+            return request;
+        };
+
+
+    public static RequestTransform GET =>
+        request =>
+        {
+            request.Method = HttpMethod.Get;
+            return request;
+        };
+
+    public static RequestTransform POST => SEND(HttpMethod.Post);
+
+    public static RequestTransform PUT => SEND(HttpMethod.Put);
+
+    public static RequestTransform DELETE => SEND(HttpMethod.Delete);
+
+    public static RequestTransform OPTIONS => SEND(HttpMethod.Options);
+
+    public static RequestTransform SEND(HttpMethod method) =>
+        request =>
+        {
+            request.Method = method;
             return request;
         };
 
@@ -58,7 +87,8 @@ public static class ApiSpecification
     public static Task And(this Task<HttpResponseMessage> response, Func<HttpResponseMessage, Task> and) =>
         response.ContinueWith(t => and(t.Result));
 
-    public static Task And<TResult>(this Task<HttpResponseMessage> response, Func<HttpResponseMessage, Task<TResult>> and) =>
+    public static Task And<TResult>(this Task<HttpResponseMessage> response,
+        Func<HttpResponseMessage, Task<TResult>> and) =>
         response.ContinueWith(t => and(t.Result));
 
     public static Task And(this Task<HttpResponseMessage> response, Func<Task> and) =>
@@ -67,62 +97,44 @@ public static class ApiSpecification
     public static Task And(this Task<HttpResponseMessage> response, Task and) =>
         response.ContinueWith(_ => and);
 
-    ///////////////////
-    ////   WHEN    ////
-    ///////////////////
-    public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> GET = SEND(HttpMethod.Get);
 
-    public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> GET_UNTIL(
-        Func<HttpResponseMessage, ValueTask<bool>> check) =>
-        SEND_UNTIL(HttpMethod.Get, check);
-
-    public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> POST = SEND(HttpMethod.Post);
-
-    public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> PUT = SEND(HttpMethod.Put);
-
-    public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> DELETE = SEND(HttpMethod.Delete);
-
-    public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> SEND(HttpMethod httpMethod) =>
-        (api, buildRequest) =>
-        {
-            var request = buildRequest();
-            request.Method = httpMethod;
-            return api.SendAsync(request);
-        };
-
-    public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> SEND_UNTIL(HttpMethod httpMethod,
-        Func<HttpResponseMessage, ValueTask<bool>> check, int maxNumberOfRetries = 5, int retryIntervalInMs = 1000) =>
-        async (api, buildRequest) =>
-        {
-            var retryCount = maxNumberOfRetries;
-            var finished = false;
-
-            HttpResponseMessage? response = null;
-            do
-            {
-                try
-                {
-                    var request = buildRequest();
-                    request.Method = httpMethod;
-
-                    response = await api.SendAsync(request);
-
-                    finished = await check(response);
-                }
-                catch
-                {
-                    if (retryCount == 0)
-                        throw;
-                }
-
-                await Task.Delay(retryIntervalInMs);
-                retryCount--;
-            } while (!finished);
-
-            response.Should().NotBeNull();
-
-            return response!;
-        };
+    // public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> GET_UNTIL(
+    //     Func<HttpResponseMessage, ValueTask<bool>> check) =>
+    //     SEND_UNTIL(HttpMethod.Get, check);
+    //
+    // public static Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> SEND_UNTIL(HttpMethod httpMethod,
+    //     Func<HttpResponseMessage, ValueTask<bool>> check, int maxNumberOfRetries = 5, int retryIntervalInMs = 1000) =>
+    //     async (api, buildRequest) =>
+    //     {
+    //         var retryCount = maxNumberOfRetries;
+    //         var finished = false;
+    //
+    //         HttpResponseMessage? response = null;
+    //         do
+    //         {
+    //             try
+    //             {
+    //                 var request = buildRequest();
+    //                 request.Method = httpMethod;
+    //
+    //                 response = await api.SendAsync(request);
+    //
+    //                 finished = await check(response);
+    //             }
+    //             catch
+    //             {
+    //                 if (retryCount == 0)
+    //                     throw;
+    //             }
+    //
+    //             await Task.Delay(retryIntervalInMs);
+    //             retryCount--;
+    //         } while (!finished);
+    //
+    //         response.Should().NotBeNull();
+    //
+    //         return response!;
+    //     };
 
     ///////////////////
     ////   THEN    ////
@@ -133,8 +145,10 @@ public static class ApiSpecification
     public static Func<HttpResponseMessage, ValueTask> BAD_REQUEST = HTTP_STATUS(HttpStatusCode.BadRequest);
     public static Func<HttpResponseMessage, ValueTask> NOT_FOUND = HTTP_STATUS(HttpStatusCode.NotFound);
     public static Func<HttpResponseMessage, ValueTask> CONFLICT = HTTP_STATUS(HttpStatusCode.Conflict);
+
     public static Func<HttpResponseMessage, ValueTask> PRECONDITION_FAILED =
         HTTP_STATUS(HttpStatusCode.PreconditionFailed);
+
     public static Func<HttpResponseMessage, ValueTask> METHOD_NOT_ALLOWED =
         HTTP_STATUS(HttpStatusCode.MethodNotAllowed);
 
@@ -152,7 +166,7 @@ public static class ApiSpecification
         {
             await CREATED(response);
             await RESPONSE_LOCATION_HEADER(locationHeaderPrefix)(response);
-            if(eTag != null)
+            if (eTag != null)
                 await RESPONSE_ETAG_HEADER(eTag, isETagWeak)(response);
         };
 
@@ -178,10 +192,11 @@ public static class ApiSpecification
     public static Func<HttpResponseMessage, ValueTask> RESPONSE_ETAG_HEADER(object eTag, bool isWeak = true) =>
         RESPONSE_HEADERS(headers =>
         {
-            headers.ETag.Should().NotBeNull("ETag response header should be defined").And.NotBe("", "ETag response header should not be empty");
+            headers.ETag.Should().NotBeNull("ETag response header should be defined").And
+                .NotBe("", "ETag response header should not be empty");
             headers.ETag!.Tag.Should().NotBeEmpty("ETag response header should not be empty");
 
-            headers.ETag.IsWeak.Should().Be(isWeak, "Etag response header should be {0}", isWeak? "Weak": "Strong");
+            headers.ETag.IsWeak.Should().Be(isWeak, "Etag response header should be {0}", isWeak ? "Weak" : "Strong");
             headers.ETag.Tag.Should().Be($"\"{eTag}\"");
         });
 
@@ -198,6 +213,7 @@ public static class ApiSpecification
 
             location.Should().StartWith(locationHeaderPrefix ?? response.RequestMessage!.RequestUri!.AbsolutePath);
         };
+
     public static Func<HttpResponseMessage, ValueTask> RESPONSE_HEADERS(params Action<HttpResponseHeaders>[] headers) =>
         response =>
         {
@@ -205,6 +221,7 @@ public static class ApiSpecification
             {
                 header(response.Headers);
             }
+
             return ValueTask.CompletedTask;
         };
 
@@ -230,7 +247,7 @@ public static class ApiSpecification
 public class ApiSpecification<TProgram>: IDisposable where TProgram : class
 {
     private readonly WebApplicationFactory<TProgram> applicationFactory;
-    private readonly HttpClient client;
+    private readonly Func<HttpClient> createClient;
 
     public ApiSpecification(): this(new WebApplicationFactory<TProgram>())
     {
@@ -239,30 +256,15 @@ public class ApiSpecification<TProgram>: IDisposable where TProgram : class
     protected ApiSpecification(WebApplicationFactory<TProgram> applicationFactory)
     {
         this.applicationFactory = applicationFactory;
-        client = applicationFactory.CreateClient();
+        createClient = applicationFactory.CreateClient;
     }
 
     public static ApiSpecification<TProgram> Setup(WebApplicationFactory<TProgram> applicationFactory) =>
         new(applicationFactory);
 
-    public async Task<HttpResponseMessage> Send(ApiRequest apiRequest) =>
-        (await Send(new[] { apiRequest })).Single();
-
-    public async Task<HttpResponseMessage[]> Send(params ApiRequest[] apiRequests)
-    {
-        var responses = new List<HttpResponseMessage>();
-
-        foreach (var request in apiRequests)
-        {
-            responses.Add(await client.Send(request));
-        }
-
-        return responses.ToArray();
-    }
-
     public GivenApiSpecificationBuilder Given(
-        params Func<HttpRequestMessage, HttpRequestMessage>[] builders) =>
-        new(client, builders);
+        params RequestTransform[][] builders) =>
+        new(createClient, builders);
 
     public async Task<HttpResponseMessage> Scenario(
         Task<HttpResponseMessage> first,
@@ -298,32 +300,32 @@ public class ApiSpecification<TProgram>: IDisposable where TProgram : class
 
     public class GivenApiSpecificationBuilder
     {
-        private readonly Func<HttpRequestMessage, HttpRequestMessage>[] given;
-        private readonly HttpClient client;
+        private readonly RequestTransform[][] given;
+        private readonly Func<HttpClient> createClient;
 
-        internal GivenApiSpecificationBuilder(HttpClient client, Func<HttpRequestMessage, HttpRequestMessage>[] given)
+        internal GivenApiSpecificationBuilder(Func<HttpClient> createClient, RequestTransform[][] given)
         {
-            this.client = client;
+            this.createClient = createClient;
             this.given = given;
         }
 
-        public WhenApiSpecificationBuilder When(Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> when) =>
-            new(client, given, when);
+        public WhenApiSpecificationBuilder When(params RequestTransform[] when) =>
+            new(createClient, given, when);
     }
 
     public class WhenApiSpecificationBuilder
     {
-        private readonly Func<HttpRequestMessage, HttpRequestMessage>[] given;
-        private readonly Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> when;
-        private readonly HttpClient client;
+        private readonly RequestTransform[][] given;
+        private readonly RequestTransform[] when;
+        private readonly Func<HttpClient> createClient;
 
         internal WhenApiSpecificationBuilder(
-            HttpClient client,
-            Func<HttpRequestMessage, HttpRequestMessage>[] given,
-            Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> when
+            Func<HttpClient> createClient,
+            RequestTransform[][] given,
+            RequestTransform[] when
         )
         {
-            this.client = client;
+            this.createClient = createClient;
             this.given = given;
             this.when = when;
         }
@@ -331,12 +333,21 @@ public class ApiSpecification<TProgram>: IDisposable where TProgram : class
         public Task<HttpResponseMessage> Then(Func<HttpResponseMessage, ValueTask> then) =>
             Then(new[] { then });
 
-        public async Task<HttpResponseMessage> Then(params Func<HttpResponseMessage, ValueTask>[] thens)
+        public Task<HttpResponseMessage> Then(params Func<HttpResponseMessage, ValueTask>[] thens) =>
+            Then(thens, default);
+
+        public async Task<HttpResponseMessage> Then(IEnumerable<Func<HttpResponseMessage, ValueTask>> thens, CancellationToken ct)
         {
-            var request = new ApiRequest(when, given);
+            using var client = createClient();
 
-            var response = await client.Send(request);
+            // Given
+            foreach (var givenBuilder in given)
+                await client.Send(givenBuilder, ct: ct);
 
+            // When
+            var response = await client.Send(when, ct: ct);
+
+            // Then
             foreach (var then in thens)
             {
                 await then(response);
@@ -348,7 +359,6 @@ public class ApiSpecification<TProgram>: IDisposable where TProgram : class
 
     public void Dispose()
     {
-        client.Dispose();
         applicationFactory.Dispose();
     }
 }
@@ -357,41 +367,42 @@ public class AndSpecificationBuilder
 {
     private readonly Lazy<Task<HttpResponseMessage>> then;
 
-    public AndSpecificationBuilder(Lazy<Task<HttpResponseMessage>> then)
-    {
+    public AndSpecificationBuilder(Lazy<Task<HttpResponseMessage>> then) =>
         this.then = then;
-    }
 
     public Task<HttpResponseMessage> Response => then.Value;
 }
 
 public class ApiRequest
 {
-    private readonly Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> send;
-    private readonly Func<HttpRequestMessage, HttpRequestMessage>[] builders;
+    private readonly RequestTransform[] builders;
 
-    public ApiRequest(
-        Func<HttpClient, Func<HttpRequestMessage>, Task<HttpResponseMessage>> send,
-        params Func<HttpRequestMessage, HttpRequestMessage>[] builders
-    )
-    {
-        this.send = send;
+    public ApiRequest(params RequestTransform[] builders) =>
         this.builders = builders;
-    }
 
-    public Task<HttpResponseMessage> Send(HttpClient httpClient)
-    {
-        HttpRequestMessage BuildRequest() =>
-            builders.Aggregate(new HttpRequestMessage(), (request, build) => build(request));
+    public HttpRequestMessage Build() =>
+        builders.Aggregate(new HttpRequestMessage(), (request, build) => build(request));
 
-        return send(httpClient, BuildRequest);
-    }
+    public static HttpRequestMessage For(params RequestTransform[] builders) =>
+        builders.Aggregate(new HttpRequestMessage(), (request, build) => build(request));
 }
 
 public static class ApiRequestExtensions
 {
-    public static Task<HttpResponseMessage> Send(this HttpClient httpClient, ApiRequest apiRequest) =>
-        apiRequest.Send(httpClient);
+    public static Task<HttpResponseMessage> Send(
+        this HttpClient httpClient,
+        ApiRequest apiRequest,
+        CancellationToken ct = default
+    ) =>
+        httpClient.SendAsync(apiRequest.Build(), ct);
+
+
+    public static Task<HttpResponseMessage> Send(
+        this HttpClient httpClient,
+        RequestTransform[] builders,
+        CancellationToken ct = default
+    ) =>
+        httpClient.SendAsync(ApiRequest.For(builders), ct);
 }
 
 public static class HttpResponseMessageExtensions
@@ -405,7 +416,7 @@ public static class HttpResponseMessageExtensions
         if (string.IsNullOrEmpty(locationHeader))
             return false;
 
-        locationHeader = locationHeader.StartsWith("/") ? locationHeader: $"/{locationHeader}";
+        locationHeader = locationHeader.StartsWith("/") ? locationHeader : $"/{locationHeader}";
 
         var start = locationHeader.LastIndexOf("/", locationHeader.Length - 1);
 
@@ -458,7 +469,8 @@ public static class HttpResponseMessageExtensions
     public static string GetETagValue(this HttpResponseMessage response) =>
         response.GetETagValue<string>();
 
-    public static async Task<T> GetResultFromJson<T>(this HttpResponseMessage response, JsonSerializerSettings? settings = null)
+    public static async Task<T> GetResultFromJson<T>(this HttpResponseMessage response,
+        JsonSerializerSettings? settings = null)
     {
         var result = await response.Content.ReadAsStringAsync();
 
